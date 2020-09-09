bf218b0398dfd47f19fe8b3bc7750f2aa531feec
Robert Koteles <koteles.roberto@gmail.com>
2020-09-09 14:02:56 +0100

    Header is done

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBpfqY_XDRsK1np1dTCb-rv2no8ckXH80o",
+    authDomain: "clone-34ebc.firebaseapp.com",
+    databaseURL: "https://clone-34ebc.firebaseio.com",
+    projectId: "clone-34ebc",
+    storageBucket: "clone-34ebc.appspot.com",
+    messagingSenderId: "36587377842",
+    appId: "1:36587377842:web:52c57574f8aa4b8c9aa5ab",
+    measurementId: "G-Q4G8V1MJZJ"
+  };
