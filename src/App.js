dd4d5585ac871f472bc7fd33f4a510ea7688b2f3
Robert Koteles <koteles.roberto@gmail.com>
2020-09-09 21:02:20 +0100

    Login page added + style

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
+import Login from "./Login";
 
 function App() {
   return (
     // BEM
     <Router>
       <div className="App">
-        <Header />
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           <Route path="/">
             {/* This is the default route, always rendered if path is wrong in URL. This Route is always at the bottom. */}
+            <Header />
             <Home />
           </Route>
         </Switch>
